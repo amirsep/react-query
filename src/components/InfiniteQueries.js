e543617f36a51578ad015ff97813b52e672c6284
amirsep <amir.sep19@gmail.com>
2024-10-07 18:08:16 +0530

    new change

--- src/components/InfiniteQueries.js
+++ src/components/InfiniteQueries.js
@@ -1,16 +1,24 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
 
-const fetchFruits = () => {
-  return axios.get(`http://localhost:4000/fruits`);
+const fetchFruits = ({ pageParam }) => {
+  return axios.get(`http://localhost:4000/fruits?_limit=4&_page=${pageParam}`);
 };
 
 const InfiniteQueries = () => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["fruits"],
-    queryFn: fetchFruits,
-  });
+  const { data, isLoading, isError, error, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ["fruits"],
+      queryFn: fetchFruits,
+      initialPageParam: 1,
+      getNextPageParam: (_lastPage, allPages) => {
+        if (allPages.length < 5) {
+          return allPages.length + 1;
+        } else {
+          return undefined;
+        }
+      },
+    });
 
   if (isLoading) {
     return (
@@ -29,17 +37,36 @@ const InfiniteQueries = () => {
 
   return (
     <div className="post-list bg-[#2f3136] rounded-lg p-5 w-full mx-auto">
-      {data?.data.map((item) => (
-        <div
-          key={item.id}
-          className="post-item bg-[#40444b] rounded-lg p-4 mb-2 transition-colors duration-300 hover:bg-[#4f545c] cursor-pointer"
-        >
-          <h3 className="post-title text-lg font-bold text-white m-0">
-            {item.name}
-          </h3>
-          <p className="post-body text-sm text-[#b9bbbe] mt-2">ID: {item.id}</p>
-        </div>
-      ))}
+      {data.pages.map((page) => {
+        return page?.data.map((fruit) => {
+          return (
+            <div
+              key={fruit.id}
+              className="post-item bg-[#40444b] rounded-lg p-4 mb-2 transition-colors duration-300 hover:bg-[#4f545c] cursor-pointer"
+            >
+              <h3 className="post-title text-lg font-bold text-white m-0">
+                {fruit.name}
+              </h3>
+              <p className="post-body text-sm text-[#b9bbbe] mt-2">
+                ID: {fruit.id}
+              </p>
+            </div>
+          );
+        });
+      })}
+
+      <button
+        onClick={fetchNextPage}
+        disabled={!hasNextPage}
+        className={`px-6 py-3  text-white font-bold rounded-lg shadow-md
+          ${
+            !hasNextPage
+              ? "bg-gray-600 cursor-not-allowed"
+              : "bg-gray-600 hover:bg-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
+          }`}
+      >
+        Load More..
+      </button>
     </div>
   );
 };
